d8d77b3dd7a825ab6612841102ef31b844aefb10
Oozudex <49617899+Oozudex@users.noreply.github.com>
2026-05-15 10:35:10 +0200

    Suppression du systeme de notificationbell

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { useAuth } from '../store/useAuth'
 import { useTheme } from '../store/useTheme'
 import { useMagasin } from '../store/useMagasin'
-import NotificationsBell from './NotificationsBell'
 import { db } from '../lib/firebase'
 import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { GLOBAL_ROLES } from '../lib/constants'
@@ -158,8 +157,6 @@ export default function Navbar() {
               : <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" /></svg>
             }
           </button>
-
-          <NotificationsBell />
 
           {/* Profile menu */}
           <div className="relative" ref={menuRef}>
